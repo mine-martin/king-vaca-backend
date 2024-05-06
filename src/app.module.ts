11350fc3e92120e3d3ef82cf3e097109d9b4debd
mine-martin <wangondumn@gmail.com>
2024-05-06 08:41:18 -0500

    Added CORS package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CarsModule } from './cars/cars.module';
+import { HousesModule } from './houses/houses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '',
+      username: '',
+      database: '',
+      autoLoadEntities: true,
+      synchronize: true,
+
+    }),
+    CarsModule, HousesModule],
 })
 export class AppModule {}
