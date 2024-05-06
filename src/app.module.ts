bd1d8751c5f39bdda1bf845fb781b9bf7cca5811
mine-martin <wangondumn@gmail.com>
2024-05-06 10:50:27 -0500

    Connected to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,35 @@ import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 import { HousesModule } from './houses/houses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: '',
-      username: '',
-      database: '',
-      autoLoadEntities: true,
-      synchronize: true,
-
+    ConfigModule.forRoot({
+      envFilePath: [`./.env}`],
+      validationSchema: configValidationSchema,
     }),
-    CarsModule, HousesModule],
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          password: configService.get('DB_PASSWORD'),
+          username: configService.get('DB_USERNAME'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+    CarsModule,
+    HousesModule,
+  ],
 })
 export class AppModule {}
