760aea95b071fc357e12eda2f0f6cf377a7fa757
mine-martin <wangondumn@gmail.com>
2024-05-07 11:05:22 -0500

    Database file: Handle database connection

---
+++ src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
