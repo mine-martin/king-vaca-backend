be801391e49f4c663927a536703513447e7aaacd
mine-martin <wangondumn@gmail.com>
2024-05-15 07:12:35 -0500

    ADD : Completed the houses module

--- src/modules/houses/houses.controller.ts
+++ src/modules/houses/houses.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { HouseDetailsService } from './houses.service';
+import { CreateHouseDetailsDto } from './dto/house_profile.dto';
+import { HouseDetails } from './entities/house.entity';
 
 @Controller('houses')
-export class HousesController {}
+export class HouseDetailsController {
+  constructor(private readonly houseDetailsService: HouseDetailsService) {}
+
+  @Get()
+  findAll(): Promise<HouseDetails[]> {
+    return this.houseDetailsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<HouseDetails> {
+    return this.houseDetailsService.findOne(id);
+  }
+
+  @Post('/new')
+  create(
+    @Body() createHouseDetailsDto: CreateHouseDetailsDto,
+  ): Promise<HouseDetails> {
+    return this.houseDetailsService.create(createHouseDetailsDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() updateHouseDetailsDto: CreateHouseDetailsDto,
+  ): Promise<HouseDetails> {
+    return this.houseDetailsService.update(id, updateHouseDetailsDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number): Promise<void> {
+    return this.houseDetailsService.remove(id);
+  }
+}
